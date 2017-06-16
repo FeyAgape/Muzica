2e193df69975090fd16e9980c9eac61a24e1ea21
Fey Ijaware <fshola4real@yahoo.co.uk>
2017-06-16 14:40:59 +0100

    included tab activity

--- app/src/main/java/com/example/android/muzica/Favourites.java
+++ app/src/main/java/com/example/android/muzica/Favourites.java
@@ -3,11 +3,11 @@ package com.example.android.muzica;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Favourite extends AppCompatActivity {
+public class Favourites extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favourite);
+        setContentView(R.layout.activity_favourites);
     }
 }
